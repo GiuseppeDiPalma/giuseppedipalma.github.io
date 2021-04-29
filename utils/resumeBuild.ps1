7453884529760f5184123e6a386feae7611b151f
Giuseppe Di Palma <g.dipalma26@studenti.unisa.it>
2021-04-29 12:36:45 +0200

    url fix

---
+++ utils/resumeBuild.ps1
@@ -0,0 +1,15 @@
+function New-ResumeBuild{
+  <#
+    .SYNOPSIS
+        Build resumè in html e pdf(for download) with pandoc.
+    .DESCRIPTION
+        Builds all *.md files inside the documents directory
+    .OUTPUTS
+        All generated html e pdf files from .md
+    .EXAMPLE
+        New-ResumeBuild
+    #>
+
+}
+
+New-ResumeBuild
