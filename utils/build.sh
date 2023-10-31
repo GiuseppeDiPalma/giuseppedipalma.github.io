6a3b35ef5cac2f8c3a62915418c410ea1492012e
Giuseppe Di Palma <dipalmagiuseppe1@gmail.com>
2023-10-31 15:50:38 +0100

    add developer certification

---
+++ utils/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Build index"
+pandoc ../index.md -s -f gfm -t html5 --template=../template/index.html --defaults=../defaults/params_index.yaml --output ../index.html
+
+echo "Build CV page"
+pandoc ../curriculumVitae/cv.md -f markdown -t html --template=../template/cv_page.html --defaults=../defaults/cv.yaml --highlight-style=breezedark --output ../curriculumVitae/cv_page.html
