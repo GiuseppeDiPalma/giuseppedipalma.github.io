aea8b95f4c1d8a2d17ddd73bb7af02dcd639afda
Giuseppe Di Palma <dipalmagiuseppe1@gmail.com>
2023-10-31 17:24:11 +0100

    update build

--- utils/build.sh
+++ utils/build.sh
@@ -5,3 +5,6 @@ pandoc ../index.md -s -f gfm -t html5 --template=../template/index.html --defaul
 
 echo "Build CV page"
 pandoc ../curriculumVitae/cv.md -f markdown -t html --template=../template/cv_page.html --defaults=../defaults/cv.yaml --highlight-style=breezedark --output ../CV.html
+
+# Usare questo per far compilare sulla base di un file md
+# pandoc ../articles/web-resources-31-03-21.md -f markdown -t html --template=../template/article.html --defaults=../defaults/cv.yaml --highlight-style=breezedark --output ../articles/CV.html
