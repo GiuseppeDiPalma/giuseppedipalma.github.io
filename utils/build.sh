33467530874371e1d6b1b9af3412bd2a96ca9602
Giuseppe Di Palma <dipalmagiuseppe1@gmail.com>
2023-10-31 16:04:01 +0100

    update title and add new page in testing

--- utils/build.sh
+++ utils/build.sh
@@ -4,4 +4,4 @@ echo "Build index"
 pandoc ../index.md -s -f gfm -t html5 --template=../template/index.html --defaults=../defaults/params_index.yaml --output ../index.html
 
 echo "Build CV page"
-pandoc ../curriculumVitae/cv.md -f markdown -t html --template=../template/cv_page.html --defaults=../defaults/cv.yaml --highlight-style=breezedark --output ../curriculumVitae/cv_page.html
+pandoc ../curriculumVitae/cv.md -f markdown -t html --template=../template/cv_page.html --defaults=../defaults/cv.yaml --highlight-style=breezedark --output ../cv_page.html
