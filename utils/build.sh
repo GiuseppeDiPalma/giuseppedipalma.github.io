ca97a3b1842ec5007a42abb904dd57e0f4a698de
Giuseppe Di Palma <dipalmagiuseppe1@gmail.com>
2024-11-18 21:33:52 +0100

    mod: refactor of code

--- utils/build.sh
+++ utils/build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 echo "Build index"
-pandoc ../index.md -s -f gfm -t html5 --template=../template/index.html --defaults=../defaults/params_index.yaml --output ../index.html
+pandoc ../index.md -s -f gfm \
+-t html5 --template=../template/index.html \
+--defaults=../defaults/params_index.yaml \
+--output ../index.html
 
 echo "Build CV page"
-pandoc ../sections/curriculumVitae/cv.md -f markdown -t html --template=../template/cv_page.html --defaults=../defaults/cv.yaml --highlight-style=breezedark --output ../CV.html
+pandoc ../sections/curriculumVitae/cv.md \
+-f markdown \
+-t html --template=../template/cv_page.html \
+--defaults=../defaults/cv.yaml \
+--highlight-style=breezedark \
+--output ../CV.html
 
 # Usare questo per far compilare sulla base di un file md
 # pandoc ../articles/web-resources-31-03-21.md -f markdown -t html --template=../template/article.html --defaults=../defaults/cv.yaml --highlight-style=breezedark --output ../articles/CV.html
